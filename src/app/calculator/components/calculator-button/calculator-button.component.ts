cae786f78e05f087a6f88cd66a9c42ae8b87064a
Fabio <fabioalbertocataldo@gmail.com>
2025-06-14 12:41:28 -0300

    double size prop added in calculator-button

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, HostBinding, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -18,9 +18,18 @@ export class CalculatorButtonComponent{
       typeof value === 'string' ? value === '' : value
   });
 
+  public isDoubleSize = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value
+  })
+
   //tengo acceso al host element que es el <calculator-button>
   // que se imprime en el inspeccionar por ej
-  @HostBinding('class.bg-indigo-700') get commandStyle() {
-    return this.isCommand();
+  // @HostBinding('class.is-command') get commandStyle() {
+  //   return this.isCommand();
+  // }
+
+  @HostBinding('class.w-2/4') get commandStyle() {
+    return this.isDoubleSize();
   }
  }
