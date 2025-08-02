4df0c8be623e22a9b9669be3a6222355de4e4d27
Fabio <fabioalbertocataldo@gmail.com>
2025-08-02 19:57:48 -0300

    More tests added to calculator component .spec.ts

--- src/app/calculator/components/calculator/calculator.component.spec.ts
+++ src/app/calculator/components/calculator/calculator.component.spec.ts
@@ -2,20 +2,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CalculatorComponent } from './calculator.component';
 import { CalculatorService } from '@/calculator/services/calculator.service';
+import { By } from '@angular/platform-browser';
+import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
 
 class MockCalculatorService {
     public resultText = jasmine.createSpy('resultText').and.returnValue('100.00');
-    public subResultText = jasmine.createSpy('subResultText').and.returnValue('0');
-    public lastOperator = jasmine.createSpy('lastOperator').and.returnValue('+');
+    public subResultText = jasmine.createSpy('subResultText').and.returnValue('20');
+    public lastOperator = jasmine.createSpy('lastOperator').and.returnValue('-');
 
     public constructNumber = jasmine.createSpy('constructNumber');
 }
-
-
 describe('CalculatorComponent', () => {
-    let fixture: ComponentFixture<CalculatorComponent>
-    let compiled: HTMLElement
-    let component: CalculatorComponent
+    let fixture: ComponentFixture<CalculatorComponent>;
+    let compiled: HTMLElement;
+    let component: CalculatorComponent;
+
+    let mockCalculatorService: MockCalculatorService;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -32,6 +34,7 @@ describe('CalculatorComponent', () => {
 
         compiled = fixture.nativeElement as HTMLElement;
         component = fixture.componentInstance;
+        mockCalculatorService = TestBed.inject(CalculatorService) as unknown as MockCalculatorService;
     });
 
     it('should create the app', () => {
@@ -40,7 +43,64 @@ describe('CalculatorComponent', () => {
 
     it('should have the current getters', () => {
         expect(component.resultText()).toBe('100.00');
-        expect(component.subResultText()).toBe('0');
-        expect(component.lastOperator()).toBe('+');
-    })
+        expect(component.subResultText()).toBe('20');
+        expect(component.lastOperator()).toBe('-');
+    });
+
+    it('should display proper calculation values', () => {
+        mockCalculatorService.resultText.and.returnValue('123');
+        mockCalculatorService.subResultText.and.returnValue('456');
+        mockCalculatorService.lastOperator.and.returnValue('*');
+
+        fixture.detectChanges();
+
+        expect(compiled.querySelector('span')?.innerText).toBe('456 *');
+
+        expect(component.resultText()).toBe('123');
+        expect(component.subResultText()).toBe('456');
+        expect(component.lastOperator()).toBe('*');
+    });
+
+    it('should have 19 calculator-button components', () => {
+        expect(component.calculatorButtons()).toBeTruthy();
+        expect(component.calculatorButtons().length).toBe(19);
+    });
+
+    it('should have 19 calculator-button with content projection', () => {
+        //we can see the buttons using the debug element
+        // const buttonsByDirective = fixture.debugElement.queryAll(
+        //     By.directive(CalculatorButtonComponent)
+        // )
+
+        //or directly using the compiled element
+        const buttons = compiled.querySelectorAll('calculator-button');
+        expect(buttons.length).toBe(19);
+
+        expect(buttons[0].textContent?.trim()).toBe('C');
+        expect(buttons[1].textContent?.trim()).toBe('+/-');
+        expect(buttons[2].textContent?.trim()).toBe('%');
+        expect(buttons[3].textContent?.trim()).toBe('÷');
+    });
+
+    it('should handle keyboard events correctly', () => {
+        const eventEnter = new KeyboardEvent('keyup', { key: 'Enter' });
+        document.dispatchEvent(eventEnter);
+
+        expect(mockCalculatorService.constructNumber).toHaveBeenCalledWith('=');
+
+        const eventESC = new KeyboardEvent('keyup', { key: 'Escape' });
+        document.dispatchEvent(eventESC);
+
+        expect(mockCalculatorService.constructNumber).toHaveBeenCalledWith('C');
+    });
+
+    it('should display result text correctly', () => {
+        mockCalculatorService.resultText.and.returnValue('123');
+        mockCalculatorService.subResultText.and.returnValue('10');
+        mockCalculatorService.lastOperator.and.returnValue('-');
+        fixture.detectChanges();
+
+        expect(component.resultText()).toBe('123');
+        expect(compiled.querySelector('#sub-result')?.textContent).toContain('10 -');
+    });
 });
